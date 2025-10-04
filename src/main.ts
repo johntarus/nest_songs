3e5cb8a462ec84172b779e31422a50786c984661
johntarus <tarusjohn96@gmail.com>
2025-10-04 17:38:24 +0300

    Added swagger config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SeedService } from './seed/seed.service';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Songs API')
+    .setDescription('API documentation for Songs App')
+    .setVersion('1.0')
+    .addBearerAuth() // if you use JWT auth
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('port') ?? 3000);
